fcecb27176c6f092bbe17c5fdcf88129c0e7274a
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-03-13 17:31:14 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Stephanie Szeto on 3/13/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RottenTomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RottenTomatoes
+//
+//  Created by Stephanie Szeto on 3/13/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
