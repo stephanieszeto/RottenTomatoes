61c61e42ad51a4c0fc6f00b6e5a7b287e3a8a1df
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-03-15 16:17:00 -0700

    table view done

---
+++ RottenTomatoes/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  RottenTomatoes
+//
+//  Created by Stephanie Szeto on 3/15/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ RottenTomatoes/MoviesViewController.m
@@ -0,0 +1,57 @@
+//
+//  MoviesViewController.m
+//  RottenTomatoes
+//
+//  Created by Stephanie Szeto on 3/15/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import "MoviesViewController.h"
+
+@interface MoviesViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation MoviesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Movies";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table View methods
+
+- (int) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"row #%d", indexPath.row];
+    
+    return cell;
+}
+
+@end
